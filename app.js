326914faf52de6a7761b076c27e86d741df3d17e
MuhammadAnsub090 <ansubwasi058@gmail.com>
2023-11-18 01:23:41 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+const div = document.querySelector('div')
+ for (let i = 1; i <= 10; i++){
+     div.innerHTML += `<br/> TABLE   <br/> <br/>`
+     for (let m = 1; m <= 10; m++) {
+         div.innerHTML += `${i} * ${m} = ${i*m} <br/>`
+   
+     }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
